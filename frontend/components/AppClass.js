ba16ad5788f5e4fc46cf9325c19508d39136167b
laurensruiz <laurenruiz97@yahoo.com>
2022-04-15 13:36:26 -0400

    made easier and cleaner code

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
+import { useEffect } from 'react'
 
 export default class AppClass extends React.Component {
   state ={
     x: 2,
     y: 2,
     steps: 0,
-    board:["", "", "", "", "B", "","", "", ""],
+    board:[
+      [null, null, null], 
+      [null, "B", null], 
+      [null, null, null]],
     email:'',
     message:''
   }
@@ -17,7 +21,10 @@ resetForm = () => {
     x: 2,
     y: 2,
     steps: 0,
-    board:["", "", "", "", "B", "","", "", ""],
+    board:[
+      [null, null, null], 
+      [null, "B", null], 
+      [null, null, null]],
     email: '',
     
   })
@@ -37,9 +44,6 @@ resetForm = () => {
       this.setState({
         ...this.state,
         message: res.data.message,
-        x: 2,
-        y: 2,
-        steps: 0,
         board:["", "", "", "", "B", "","", "", ""],
         email: '',
       })
@@ -60,105 +64,64 @@ resetForm = () => {
     this.postOnSubmit();
     
   }
+
+onClick = (varX, varY) => {
   
-//Left up down
-
-arraymove =(arr, fromIndex, toIndex) =>{
-  var element = arr[fromIndex];
-  arr.splice(fromIndex, 1);
-  arr.splice(toIndex, 0, element);
-}
-
-onClickL = () => {
-  const indexB = this.state.board.indexOf('B')
-  const boardArray = this.state.board
-  if (indexB === 1 || indexB === 2 ||indexB === 4 || indexB === 5 ||indexB === 7 || indexB === 8 ){
-    this.arraymove(boardArray, indexB, (indexB-1))
-  }
-  let errorMessage =''
-    if(indexB === 0 || indexB === 3 || indexB === 6) {
-      errorMessage = 'You can\'t go left'
+    if(this.state.x + varX > 3) {
+      this.setState( {
+        ...this.state,
+        message: "You can't go right"
+      })
+    } else if (this.state.x + varX < 1) {
+      this.setState( {
+        ...this.state,
+        message: "You can't go left"
+      })
+    } else if (this.state.y + varY > 3) {
+      this.setState( {
+        ...this.state,
+        message: "You can't go down"
+      })
+    } else if (this.state.y + varY < 1) {
+      this.setState( {
+        ...this.state,
+        message: "You can't go up"
+      })
+    } else {
+      const idxX = (this.state.x-1);
+      const idxY = (this.state.y-1);
+      const idxXB = (idxX + varX);
+      const idxYB = (idxY + varY)
+      const updatedBoard = [...this.state.board];
+      updatedBoard[idxY][idxX] = null
+      updatedBoard[idxYB][idxXB] = 'B'
+      console.log(updatedBoard)
+      this.setState({
+        ... this.state,
+        x: this.state.x+ varX,
+        y: this.state.y+ varY,
+        steps: this.state.steps + 1,
+        board: updatedBoard,
+        message: ''
+      })
     }
-    this.setState({
-      ...this.state,
-      steps: this.state.steps + 1,
-      x: this.state.x <= 3 && this.state.x > 1? this.state.x -1: this.state.x,
-      y: this.state.y,
-      board: boardArray,
-      message: errorMessage
-       
-    })
-  }
-
-onClickR = () => {
-  const indexB = this.state.board.indexOf('B')
-  const boardArray = this.state.board
-  if (indexB === 0 || indexB === 1 ||indexB === 3 || indexB === 4 ||indexB === 6 || indexB === 7 ){
-    this.arraymove(boardArray, indexB, (indexB+1))
-  }
-  let errorMessage =''
-    if(indexB === 2 || indexB === 5 || indexB === 8) {
-      errorMessage = 'You can\'t go right'
-    }
-    this.setState({
-      ...this.state,
-      steps: this.state.steps + 1,
-      x: this.state.x < 3 && this.state.x >= 1? this.state.x +1: this.state.x,
-      y: this.state.y,
-      message: errorMessage
-    })
-  }
-
-  onClickU = () => {
-    const indexB = this.state.board.indexOf('B')
-    const boardArray = this.state.board
-    if (indexB === 3 || indexB === 4 || indexB === 5 || indexB === 6 || indexB === 7 || indexB === 8 ){
-      this.arraymove(boardArray, indexB, (indexB-3))
-    }
-    let errorMessage =''
-    if(indexB === 0 || indexB === 1 || indexB === 2) {
-      errorMessage = 'You can\'t go up'
-    }
-    this.setState({
-      ...this.state,
-      steps: this.state.steps + 1,
-      x: this.state.x,
-      y: this.state.y <= 3 && this.state.y > 1? this.state.y -1 : this.state.y,
-      message: errorMessage
-    })
-  }
-
-  onClickD = () => {
-    const indexB = this.state.board.indexOf('B')
-    const boardArray = this.state.board
-    if (indexB === 1 || indexB === 2 || indexB === 3 || indexB === 4 || indexB === 5 || indexB === 0 ){
-      this.arraymove(boardArray, indexB, (indexB+3))
-    }
-    let errorMessage =''
-    if(indexB === 6 || indexB === 7 || indexB === 8) {
-      errorMessage = 'You can\'t go down'
-    }
-    this.setState({
-      ...this.state,
-      steps: this.state.steps + 1,
-      x: this.state.x,
-      y: this.state.y < 3 && this.state.y >= 1? this.state.y +1 : this.state.y,
-      message: errorMessage
-    })
   }
   
+  
+
 
   render() {
     const { className } = this.props
-    
+    const flattenedBoard = this.state.board.flat();
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
-          <h3 id="steps">You moved {this.state.steps} times</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.x}, ${this.state.y})`}</h3>
+          {this.state.steps === 1 ? <h3 id="steps">You moved 1 time</h3> : <h3 id="steps">You moved {this.state.steps} times</h3> } 
+          {/* account for 1 time */}
         </div>
         <div id="grid">
-        {this.state.board.map(val=>{
+        {flattenedBoard.map(val=>{
           return <div className={`${val? 'square active' : 'square'}`}>{val}</div>
         })}
         </div>
@@ -166,10 +129,10 @@ onClickR = () => {
           <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button onClick={this.onClickL} id="left">LEFT</button>
-          <button onClick={this.onClickU} id="up">UP</button>
-          <button onClick={this.onClickR} id="right">RIGHT</button>
-          <button onClick={this.onClickD} id="down">DOWN</button>
+          <button onClick={() => this.onClick(-1,0)} id="left">LEFT</button>
+          <button onClick={() => this.onClick(0, -1)} id="up">UP</button>
+          <button onClick={() => this.onClick(1, 0)} id="right">RIGHT</button>
+          <button onClick={() => this.onClick(0, 1)} id="down">DOWN</button>
           <button onClick={this.resetForm} id="reset">reset</button>
         </div>
         <form id="form-wrapper" onSubmit={this.onSubmit}>
